1627af3cda938c411c2ebe424cac3dce25053cda
LinaGre4ka <120452311+LinaGre4ka@users.noreply.github.com>
2023-06-08 17:33:10 +0300

    Add files via upload

---
+++ JS/burger.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("burg").addEventListener("click", function() {
+        document.querySelector("header").classList.toggle("open")
+    })
+})
